5e534206fc241c8f8f8c49e152bbe075fb2c1f7c
bit <bit@DESKTOP-74J9BGV>
2019-01-02 10:51:44 +0900

    add

--- src/paint/MainApp.java
+++ src/paint/MainApp.java
@@ -6,12 +6,38 @@ public class MainApp {
 		Point p1 = new Point();
 		p1.setX(100);
 		p1.setY(200);
-		
+		drawPoint(p1);
 		p1.show();
 		
 		Point p2 = new Point(50,50);//생성자 오버로딩
 		p2.show();
 		p2.show(false);
+		drawPoint(p2);
+		
+		
+		//ColorPoint p3= new ColorPoint();
+		Point p3= new ColorPoint();
+		p3.setX(100);
+		p3.setY(50);
+		//(ColorPoint)p3.setColor("red");//캐스팅
+		((ColorPoint)p3).setColor("red");//캐스팅
+		//p3.show(true);//Point에서 boolean에서 show자체를 불러와서 -재사용-
+		//p3.show(false);
+		drawPoint(p3);
+		
+		Triangle triangle = new Triangle();
+		drawtriangle(triangle);
+		
+		Rect rect = new Rect();
+		drawrect(rect);
 	}
-
+	public static void drawPoint(Point point) {
+		point.show();
+	}
+	public static void drawtriangle(Triangle triangle) {
+		triangle.draw();
+	}
+	public static void drawrect(Rect rect) {
+		rect.draw();
+	}
 }
---
+++ src/paint/Shape.java
@@ -0,0 +1,29 @@
+package paint;
+
+public abstract class Shape {
+	private String lineColor;
+	private String fillColor;
+
+	public String getLineColor() {
+		return lineColor;
+	}
+
+	public void setLineColor(String lineColor) {
+		this.lineColor = lineColor;
+	}
+
+	public String getFillColor() {
+		return fillColor;
+	}
+
+	public void setFillColor(String fillColor) {
+		this.fillColor = fillColor;
+	}
+	
+	public abstract void draw();
+	//abstract 미완성 메소드
+	//를 가지고있는 클래스도 붙여줘야 오류가 사라짐
+	
+	
+	
+}
